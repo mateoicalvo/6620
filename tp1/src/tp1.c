155d2db7d46cdf6f7696d3e85dcd6026ce896c7d
Mateo <macalvo@fi.uba.ar>
2020-11-07 00:31:50 +0000

    finalizo logica de ejecucion en el main

--- tp1/src/tp1.c
+++ tp1/src/tp1.c
@@ -3,10 +3,8 @@
 int main(int argc, const char *argv[]) {
     
     hasheador_t hasheador;
-    //TODO: inicializar, correr y devolver
     hasheador_inicializar(&hasheador, argc-1, argv);
-    hasheador_correr(&hasheador);
+    int resultado_ejecucion = hasheador_correr(&hasheador);
     hasheador_destruir(&hasheador);
-
-    return 0;
+    return resultado_ejecucion;
 }
