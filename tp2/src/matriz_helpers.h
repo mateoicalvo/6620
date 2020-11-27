60b5a18a1f12440069a9d6454166f46e9d9b4961
Mateo <macalvo@fi.uba.ar>
2020-11-27 18:37:29 +0000

    Muevo arcvhiso

--- tp2/src/matriz_helpers.h
+++
@@ -1,7 +0,0 @@
-#include <stddef.h>
-
-void matriz_multiplicar_vector_naive_c(double* A, double* B, \
-    double* resultado, size_t n);
-
-void matriz_multiplicar_naive(double* A, double* B, \
-    double* resultado, size_t n);
