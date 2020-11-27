7b1eea3c6fa81a05067390d533ddfe7d4f7eeb12
Mateo <macalvo@fi.uba.ar>
2020-11-27 21:21:39 +0000

    Continuo desarrollo de script de automatizacion

--- tp2/benchmark.sh
+++ tp2/benchmark.sh
@@ -10,18 +10,22 @@ else #assembler
     cp $1 asm/mmult.S
 fi
 gcc -g -c asm/mmult.* -o obj/mmult.o
-if [[ "$2" == "yes" ]]
+if [[ "$2" == "clear_cache=yes" ]]
 then
     sed -i '/#define CLEAR_CACHE 0/c\#define CLEAR_CACHE 1' src/benchmark.c
 else
     sed -i '/#define CLEAR_CACHE 1/c\#define CLEAR_CACHE 0' src/benchmark.c
 fi
+
 make
 
+dim_actual=1
 for filename in $3/*.txt; do
     /opt/valgrind/bin/valgrind --tool=cachegrind bin/benchmark > /dev/null < $filename
-    /opt/valgrind/bin/cg_annotate cachegrind.out.* /root/tp2/asm/mmult.*
+    echo -n $dim_actual" " >> $4
+    echo -n $2" " >> $4
+    echo -n $1" " >> $4
+    /opt/valgrind/bin/cg_annotate cachegrind.out.* /root/tp2/asm/mmult.* | tail -n2 | head -n1 >> calcus.txt    
     rm cachegrind.out.*
+    ((dim_actual=dim_actual+1))
 done
-
-#/opt/valgrind/bin/cg_annotate cachegrind.out.2050 root/tp2/asm/mmult.* asterisk y despues rm
