6f166fd8443d62501c05613c0b1a51039e494d19
Mateo <macalvo@fi.uba.ar>
2020-11-28 21:27:50 +0000

    Avanzo en desarrollo de automatizacion

--- tp2/benchmark.sh
+++ tp2/benchmark.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+RUTA_FUNCION=$1
+CLEAR_CACHE=$2
+RUTA_INPUTS=$3
+RUTA_LOGS=$4
+ARCHIVO_LOG_RESULTADOS=$4/log.txt
+FLAGS_CACHEGRIND_I=$5
+FLAGS_CACHEGRIND_D=$6
 mkdir -p obj
 mkdir -p asm
-if [[ "$1" == *.c ]]
+mkdir -p $RUTA_LOGS
+
+if [[ "$RUTA_FUNCION" == *.c ]]
 then
-    gcc -g -S $1 -o asm/mmult.s;
+    gcc -g -S $RUTA_FUNCION -o asm/mmult.s;
     sed -i '/.file 1/,+1d' asm/mmult.s
 else #assembler
-    cp $1 asm/mmult.S
+    cp $RUTA_FUNCION asm/mmult.S
 fi
+
+REAL_PATH=$(realpath asm/mmult.*)
+
 gcc -g -c asm/mmult.* -o obj/mmult.o
-if [[ "$2" == "clear_cache=yes" ]]
+if [[ "$CLEAR_CACHE" == "clear_cache=yes" ]]
 then
     sed -i '/#define CLEAR_CACHE 0/c\#define CLEAR_CACHE 1' src/benchmark.c
 else
@@ -19,13 +31,16 @@ fi
 
 make
 
-dim_actual=1
-for filename in `ls $3/*.txt | sort -V`; do
-    /opt/valgrind/bin/valgrind --tool=cachegrind bin/benchmark > /dev/null < $filename
-    echo -n $dim_actual" " >> $4
-    echo -n $2" " >> $4
-    echo -n $1" " >> $4
-    /opt/valgrind/bin/cg_annotate cachegrind.out.* /root/tp2/asm/mmult.* | tail -n2 | head -n1 >> $4  
+iteracion=1
+for filename in `ls $RUTA_INPUTS/*.txt | sort -V`; do
+    /opt/valgrind/bin/valgrind --tool=cachegrind $FLAGS_CACHEGRIND_I $FLAGS_CACHEGRIND_D bin/benchmark > /dev/null < $filename
+    echo -n $iteracion" " >> $ARCHIVO_LOG_RESULTADOS
+    echo -n $filename" " >> $ARCHIVO_LOG_RESULTADOS
+    echo -n $CLEAR_CACHE" " >> $ARCHIVO_LOG_RESULTADOS
+    echo -n $RUTA_FUNCION" " >> $ARCHIVO_LOG_RESULTADOS
+    echo -n $FLAGS_CACHEGRIND_I $FLAGS_CACHEGRIND_D" " >> $ARCHIVO_LOG_RESULTADOS
+    /opt/valgrind/bin/cg_annotate cachegrind.out.* $REAL_PATH | tail -n2 | head -n1 >> $ARCHIVO_LOG_RESULTADOS 
+    #dim1_chachegrid_log.tst
     rm cachegrind.out.*
-    ((dim_actual=dim_actual+1))
+    ((iteracion=iteracion+1))
 done
